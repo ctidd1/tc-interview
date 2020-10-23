32a8d0847be1f7d16dcd457762fa80dc0024f6a6
Cameron Tidd <ctidd@technicallycreative.com>
2020-10-23 16:16:30 -0400

    Initial commit

---
+++ tc-nifi-interview/src/main/java/com/technicallycreative/MyProcessor.java
@@ -0,0 +1,14 @@
+package com.technicallycreative;
+
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.exception.ProcessException;
+
+public class MyProcessor extends AbstractProcessor {
+
+    @Override
+    public void onTrigger(ProcessContext processContext, ProcessSession processSession) throws ProcessException {
+
+    }
+}
